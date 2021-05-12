a78b5a24e159f96b2ada07b63b8d3c6b9262edb7
Aerith_ <chutirat.earth76@mail.kmutt.ac.th>
2021-05-12 23:00:25 +0700

    edited class MySql

--- Backend/src/main/java/com/earth/inventorysystem/util/MySqlConnection.java
+++ Backend/src/main/java/com/earth/inventorysystem/util/MySqlConnection.java
@@ -5,22 +5,19 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class MySqlConnection {
-    private static final String db_url = "jdbc:mysql://csproject.sit.kmutt.ac.th:3306/db63130500205";
-
+    private static final String db_url = "jdbc:mysql://mysql.cslab.bsthun.com:1205/inventory_system";
 
     private static Connection connection;
 
     public static Connection getConnection() throws SQLException {
-        if (!connection.isValid(200)) {
+        if (connection == null || !connection.isValid(200)) {
             return reConnect();
         }
         return connection;
     }
 
-
-
     public static Connection reConnect() throws SQLException {
-        connection = DriverManager.getConnection(db_url, "63130500205", "Chutirat_13055");
+        connection = DriverManager.getConnection(db_url, "aerith_1026", "1026");
         return connection;
     }
 }

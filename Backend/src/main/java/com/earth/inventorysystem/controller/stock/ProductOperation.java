34890ae443964051d5c8fea6a48fd8b4c699ec94
Aerith_ <chutirat.earth76@mail.kmutt.ac.th>
2021-05-12 23:01:43 +0700

    edited ProductOperation class

--- Backend/src/main/java/com/earth/inventorysystem/controller/stock/ProductOperation.java
+++ Backend/src/main/java/com/earth/inventorysystem/controller/stock/ProductOperation.java
@@ -1,6 +1,7 @@
 package com.earth.inventorysystem.controller.stock;
 
-import com.mysql.cj.x.protobuf.MysqlxPrepare;
+import com.earth.inventorysystem.util.MySqlConnection;
+import org.springframework.web.bind.annotation.*;
 
 import java.sql.*;
 import java.util.ArrayList;
@@ -8,40 +9,23 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
-import com.earth.inventorysystem.util.MySqlConnection;
-import org.springframework.web.bind.annotation.*;
-
 @RestController
 @RequestMapping("/stock")
 
 public class ProductOperation {
-    Connection connection;
-    PreparedStatement preparedStatement;
-    ResultSet resultSet;
-    Product product;
 
     @PostMapping(path = "/incresestock")
 
-    public Map<String, Object>  incresestock(@RequestParam  int stock_id, @RequestParam int updateAmount) {
-        int originalAmount;
-        int newAmount = 0;
+    public Map<String, Object> incresestock(@RequestParam int stock_id, @RequestParam int updateAmount) {
         Map<String, Object> res = new HashMap<>();
 
         try {
-            connection = MySqlConnection.getConnection();
-            preparedStatement = connection.prepareStatement("SELECT amount FROM `stock`");
-            originalAmount = resultSet.getInt("stock_id");
-            preparedStatement = connection.prepareStatement("UPDATE stock SET Amount = Amount+?   WHERE stock_id = ? ");
+            Connection connection = MySqlConnection.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement("UPDATE stock SET amount = amount + ? WHERE stock_id = ?");
             preparedStatement.setInt(1, updateAmount);
             preparedStatement.setInt(2, stock_id);
             preparedStatement.execute();
 
-            preparedStatement = connection.prepareStatement("SELECT * FROM stock WHERE stock_id = ?");
-            preparedStatement.setInt(1, stock_id);
-            ResultSet rs = preparedStatement.executeQuery();
-            rs.next();
-            res.put("amount",rs.getInt("amount"));
-            return res;
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         } finally {
@@ -57,7 +41,7 @@ public class ProductOperation {
     }
 
     @PostMapping(path = "/decresestock")
-    public Map<String, Object>  decresestock(@RequestParam int stock_id, @RequestParam int updateAmount) {
+    public Map<String, Object> decresestock(@RequestParam int stock_id, @RequestParam int updateAmount) {
         int originalAmount = 0;
         int newAmount = 0;
         Map<String, Object> res = new HashMap<>();
@@ -75,7 +59,7 @@ public class ProductOperation {
             preparedStatement.setInt(1, stock_id);
             ResultSet rs = preparedStatement.executeQuery();
             rs.next();
-            res.put("amount",rs.getInt("amount"));
+            res.put("amount", rs.getInt("amount"));
             return res;
         } catch (SQLException throwables) {
             throwables.printStackTrace();
@@ -92,21 +76,22 @@ public class ProductOperation {
 
 
     }
-    @PostMapping(path = "/buying")
-    public Map<String, Object>  buying(@RequestParam int stock_id,@RequestParam int amount,@RequestParam int user_id){
+
+    @PostMapping(path = "/update ")
+    public Map<String, Object> buying(@RequestParam int stock_id, @RequestParam int amount, @RequestParam int user_id) {
         Timestamp now = new Timestamp(new Date().getTime());
         Map<String, Object> res = new HashMap<>();
 
         try {
             connection = MySqlConnection.getConnection();
-            preparedStatement = connection.prepareStatement("INSERT into buying (date,amount,user_id,stock_id) values (?,?,?,?) ");
+            preparedStatement = connection.prepareStatement("INSERT into  update (date,amount,user_id,stock_id) values (?,?,?,?) ");
             preparedStatement.setTimestamp(1, now);
             preparedStatement.setInt(2, amount);
             preparedStatement.setInt(3, user_id);
             preparedStatement.setInt(4, stock_id);
             preparedStatement.execute();
 
-            res.put("success",true);
+            res.put("success", true);
             return res;
 
         } catch (SQLException throwables) {
@@ -124,26 +109,26 @@ public class ProductOperation {
     }
 
     @GetMapping(path = "/user")
-    public Map<String, Object> user(@RequestParam int user_id){
+    public Map<String, Object> user(@RequestParam int user_id) {
         Map<String, Object> res = new HashMap<>();
 
-        try{
+        try {
             connection = MySqlConnection.getConnection();
             preparedStatement = connection.prepareStatement("SELECT * FROM buying INNER JOIN stock ON buying.stock_id = stock.stock_id WHERE buying.user_id = ? ");
-            preparedStatement.setInt(1,user_id);
+            preparedStatement.setInt(1, user_id);
             ResultSet rs = preparedStatement.executeQuery();
-            ArrayList <Object> arr = new ArrayList<>();
+            ArrayList<Object> arr = new ArrayList<>();
 
-            while(rs.next()){
+            while (rs.next()) {
                 Map<String, Object> order = new HashMap<>();
-                order.put("date",rs.getDate("date"));
-                order.put("user_id",rs.getInt("user_id"));
-                order.put("stock_name",rs.getString("stock_name"));
-                order.put("amount",rs.getInt("amount"));
+                order.put("date", rs.getDate("date"));
+                order.put("user_id", rs.getInt("user_id"));
+                order.put("stock_name", rs.getString("stock_name"));
+                order.put("amount", rs.getInt("amount"));
                 arr.add(order);
 
             }
-            res.put("order",arr);
+            res.put("order", arr);
             return res;
 
         } catch (SQLException throwables) {

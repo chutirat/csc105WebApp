24fe15139324f9f26c7e7a92ba49f38938d88fe0
Aerith_ <chutirat.earth76@mail.kmutt.ac.th>
2021-05-14 20:11:42 +0700

    edited router

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -10,35 +10,35 @@ import Dashboard from "./component/Dashboard";
 import History from "./pages/History";
 
 export default function App() {
-	return (
-		<Router>
-			<Navbar />
+  return (
+    <Router>
+      <Navbar />
 
-			<Box marginTop="64px">
-				<Switch>
-					<Route path="/login">
-						<Login />
-					</Route>
-					<Route path="/dashboard">
-						<Dashboard />
-					</Route>
-					<Route path="/next/right1">
-						<ProductList1 title="All products" list="list" />
-					</Route>
-					<Route path="/next/right2">
-						<ProductList1 title="In-stock products" list="inlist" />
-					</Route>
-					<Route path="/next/right3">
-						<ProductList1 title="Out of stock products" list="outlist" />
-					</Route>
-					<Route path="/history">
-						<History />
-					</Route>
-					<Route path="/">
-						<Home />
-					</Route>
-				</Switch>
-			</Box>
-		</Router>
-	);
+      <Box marginTop="64px">
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+          <Route path="/next/right1">
+            <ProductList1 title="All products" list="list" />
+          </Route>
+          <Route path="/next/right2">
+            <ProductList1 title="In-stock products" list="inlist" />
+          </Route>
+          <Route path="/next/right3">
+            <ProductList1 title="Out of stock products" list="outlist" />
+          </Route>
+          <Route path="/history">
+            <History />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Box>
+    </Router>
+  );
 }
